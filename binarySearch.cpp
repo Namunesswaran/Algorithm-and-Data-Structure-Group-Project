fa6a583d62e7b38ddace47f8db6906c32c54fd60
farisimba <farisimbaa@gmail.com>
2025-11-27 21:32:45 +0800

    Update binarySearch.cpp

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -5,32 +5,17 @@ using namespace std;
 int main(int arr[], int size, int target) {
     int left = 0;
     int right = size - 1;
-    int mid;
-    bool found = false;
 
-    // binary search
     while (left <= right) {
-        mid = (left + right) / 2;
+        int mid = (left + right) / 2;
 
-        if (arr[mid] == target) {
-            found = true;
-            break;
-        }
+        if (arr[mid] == target)
+            return mid;
 
-        // if the book title is bigger alphabetically
-        if (arr[mid] < target) {
+        if (arr[mid] < target)
             left = mid + 1;
-        } 
-        else {
+        else
             right = mid - 1;
-        }
     }
-
-    if (found) {
-        cout << "\nBook found at position: " << mid << endl;
-    } else {
-        cout << "\nBook not found in the library." << endl;
-    }
-
-    return 0;
+    return -1;
 }
